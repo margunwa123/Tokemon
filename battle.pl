fb767bc196cd49e824a7136fbcb77558ef57154e
Mario Gunawan <margunwa123@yahoo.com>
2019-11-15 15:12:23 +0700

    ngeberesin debug

--- battle.pl
+++ battle.pl
@@ -1,8 +1,10 @@
 /* File untuk saat pokemon bertarung */
 :- dynamic(lawan/5).
 :- dynamic(chosenToke/2).
+:- dynamic(runorfight).
 :- include('tokemon.pl').
 
+%run :- inbattle,
 pick(X):- inbattle, toke(X,_,_,_,_), asserta(chosenToke(X,1)), 
          write('You : "Saya memilih kamu,'),write(X),write('."'),nl,nl,!.
 pick(X):- \+(toke(X,_,_,_,_)), write('Kamu tidak memiliki pokemon tersebut!'), nl.
@@ -66,21 +68,53 @@ cekhealthP :- chosenToke(X,_), toke(X,HPM,_,_,_), HPM > 0,
 cekhealthL :- lawan(Y,HPL,_,_,_), HPL < 0, 
               write(Y), write(' pingsan! Apakah kamu mau menangkapnya?'),nl,nl,
               retract(inbattle),!.        
-cekhealthL :- lawan(Y,HPL,_,_,_), HPL > 0, 
+cekhealthL :- lawan(_,HPL,_,_,_), HPL > 0, 
               life, !, attacked.        
 
 cektokemon :- write('Kamu masih memiliki sisa Tokemon!'), nl,
               write('Pilih Tokemon sekarang!'), asserta(inbattle),!.                             
 cektokemon :- \+toke(X,_,_,_,_), lose,!.
 
-change(A) :- inbattle, /+ toke(A,_,_,_,_),
+change(A) :- inbattle, \+(toke(A,_,_,_,_)),
              write('Kamu tidak memiliki Tokemon tersebut!'), nl, !.
 change(A) :- inbattle, chosenToke(X,_), A =:= X, 
              write('Kamu sedang memakai Tokemon '), write(A), nl, !.
-change(A) :- inbattle, chosenToke(X,_), A =/= X,
+change(A) :- inbattle, chosenToke(X,_), A \= X,
              write('Kembalilah '), write(A), nl,
              retract(chosenToke(X,_)), asserta(chosenToke(A,_)),
              write('Maju, '), write(A), nl, !.
 change(A) :- write('Kamu tidak sedang bertarung!'),nl,!.
 
-
+%tiap movement di cek kondisinya
+cekKondisi :-
+    player(T,L),
+    gym(T,L),
+    write('Kamu sekarang berada dalam Gym, ketik "heal." untuk menyembuhkan semua tokemonmu.'),!.
+cekKondisi :- 
+    cekToke(Byk),
+    Byk < 3,
+    get_normal_number,
+    randomNum(X),
+    id(Nama,X),
+    tokemon(Nama,A,B,C,Type),asserta(lawan(Nama,A,B,C,Type)),
+    write('Kamu telah bertemu dengan sebuah tokemon bernama '),write(Nama),write(' dengan tipe '),write(Type),nl,
+    write('Apa yang akan kamu lakukan???'),nl,
+    write('1. Lawan. - Bertarung melawan tokemon liar'),nl,
+    write('2. Lari.  - Melarikan diri dari tokemon'),nl,
+    asserta(inbattle),!.
+%player tidak bisa menemukan legendary tokemon bila tokemonnya < 3
+cekKondisi :- 
+    cekToke(Byk),
+    Byk >= 3,
+    get_random_number,
+    randomNum(X),
+    id(Nama,X),!,
+    tokemon(Nama,A,B,C,Type),
+    write('Kamu telah bertemu dengan sebuah'),legendary(Nama) -> (write(' legendary ')),write(' tokemon bernama '),write(Nama),write(' dengan tipe '),write(Type),nl,
+    write('Apa yang akan kamu lakukan???'),nl,
+    write('1. Lawan. - Bertarung melawan tokemon liar'),nl,
+    write('2. Lari.  - Melarikan diri dari tokemon'),nl,
+    asserta(inbattle),
+    asserta(lawan(Nama,A,B,C,Type)),!.
+cekKondisi :-
+    write('Kamu tidak menemukan apa apa di petak ini'),!.
